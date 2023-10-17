6f735b88d23bbfe8115cd995870bd0bdd0116ba7
Me <rhgs77@yahoo.com>
2023-10-17 19:27:00 -0400

    Corrected for 0.217.24.  Some of the version changes in the game code made the best behavior ambiguous so I added a config file to make them user confiugrable

--- FriendlyTurrets.cs
+++ FriendlyTurrets.cs
@@ -1,25 +1,55 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 namespace FriendlyTurrets
 {
-    [BepInPlugin("ByteArtificer.FriendlyTurrets", "Friendly Ballista", "1.0.1")]
+    [BepInPlugin("ByteArtificer.FriendlyTurrets", "Friendly Ballista", "1.0.3")] 
     [BepInProcess("valheim.exe")]
     public class FriendlyTurrets : BaseUnityPlugin
     {
         private readonly Harmony harmony = new Harmony("ByteArtificer.FriendlyTurrets");
         public static ManualLogSource logger;
+        public static bool debugLogging = false;
+
+        private static ConfigEntry<bool> attackPassivesConfig;
+        private static ConfigEntry<bool> attackPlayersConfig;
+        private static ConfigEntry<bool> attackTamedConfig;
+        private static ConfigEntry<bool> attackBabiesConfig;
 
         public void Awake()
         {
             logger = Logger;
             harmony.PatchAll();
+
+
+            attackPassivesConfig = Config.Bind("General",      // The section under which the option is shown
+                                "attackPassivesConfig",  // The key of the configuration option in the configuration file
+                                false, // The default value
+                                "True means passive creatures get shot at"); // Description of the option to show in the config file
+
+                        attackPlayersConfig = Config.Bind("General",
+                                                "attackPlayersConfig",
+                                                false,
+                                                "True means Players get shot at");
+            attackTamedConfig = Config.Bind("General",
+                                         "attackTamedConfig",
+                                         false,
+                                         "True means tamed creatures get shot at");
+
+            attackBabiesConfig = Config.Bind("General",
+                                  "attackBabiesConfig",
+                                  false,
+            "True means baby creatures get shot at");
         }
 
         [HarmonyPatch(typeof(Turret), nameof(Turret.UpdateTarget))]
@@ -29,7 +59,7 @@ namespace FriendlyTurrets
             {
                 logger.LogMessage("Friendly turrets patching...");
                 var foundClosestCreatureCall = false;
-                
+
                 var codes = new List<CodeInstruction>(instructions);
                 for (var j = 0; j < codes.Count; j++)
                 {
@@ -59,26 +89,39 @@ namespace FriendlyTurrets
             }
         }
 
-        [HarmonyPatch(typeof(Turret), nameof(Turret.ShootProjectile))]
-        public static class Turret_ShootProjectile_Prefix
-        {
-            static void Prefix(ref Character ___m_target, ref ZNetView ___m_nview)
-            {
-                if (IsValidTarget(___m_target, true))
-                    return;
 
-                logger.LogError("Firing on illegal target, destorying turret ammo for safety.");
-                ___m_nview.GetZDO().Set("ammo", 0);
-            }
-        }
-
-        public static Character FindClosestPlayerUnfriendlyCreature(Transform me, Vector3 eyePoint, float hearRange, float viewRange, float viewAngle, bool alerted, bool mistVision, bool includePlayers = true, bool includeTamed = true, List<Character> onlyTargets = null)
+        public static Character FindClosestPlayerUnfriendlyCreature(Transform me, Vector3 eyePoint, float hearRange, float viewRange, float viewAngle, bool alerted, bool mistVision, bool passiveAggresive, bool includePlayers, bool includeTamed, List<Character> onlyTargets = null)
         {
+
+
             List<Character> allCharacters = Character.GetAllCharacters();
             Character character = null;
             float num = 99999f;
             foreach (Character item in allCharacters)
             {
+                BaseAI baseAI = item.GetBaseAI();
+
+
+                if (FriendlyTurrets.debugLogging) logger.LogMessage(
+                    $"{item}  FriendlyTurrets.attackPlayersConfig.Value=" + FriendlyTurrets.attackPlayersConfig.Value +
+                    " FriendlyTurrets.attackPassivesConfig.Value=" + FriendlyTurrets.attackPassivesConfig.Value
+                    + " FriendlyTurrets.attackTamedConfig.Value=" + FriendlyTurrets.attackTamedConfig.Value
+                    + " FriendlyTurrets.attackBabiesConfig.Value=" + FriendlyTurrets.attackBabiesConfig.Value
+                    + " baseAI=" + baseAI +
+                    " (!attackPlayers && item is Player)=" + (!FriendlyTurrets.attackPlayersConfig.Value && item is Player) +
+                    " (!FriendlyTurrets.attackTamedConfig.Value  && item.IsTamed())=" + (!FriendlyTurrets.attackTamedConfig.Value && item.IsTamed()) +
+                    " item.GetBaseAI().m_passiveAggresive)=" + (baseAI ? baseAI.m_passiveAggresive : false));
+
+                if (baseAI == null || (!FriendlyTurrets.attackPassivesConfig.Value && baseAI.m_passiveAggresive) || (!FriendlyTurrets.attackTamedConfig.Value && item.IsTamed()) || (!FriendlyTurrets.attackPlayersConfig.Value && item is Player) || (!FriendlyTurrets.attackBabiesConfig.Value && item.GetComponents<Growup>().Any()))
+                {
+                    continue;
+                }
+
+                if (item.IsDead()) // dont beat a dead horse
+                {
+                    continue;
+                }
+
                 if (onlyTargets != null && onlyTargets.Count > 0)
                 {
                     bool flag = false;
@@ -90,20 +133,15 @@ namespace FriendlyTurrets
                             break;
                         }
                     }
-
                     if (!flag)
                     {
                         continue;
                     }
                 }
+                
 
-                if (!IsValidTarget(item, false))
-                {
-                    continue;
-                }
 
-                BaseAI baseAI = item.GetBaseAI();
-                if ((!(baseAI != null) || !baseAI.IsSleeping()) && BaseAI.CanSenseTarget(me, eyePoint, hearRange, viewRange, viewAngle, alerted, mistVision, item))
+                if ((!(baseAI != null) || !baseAI.IsSleeping()) && BaseAI.CanSenseTarget(me, eyePoint, hearRange, viewRange, viewAngle, alerted, mistVision, item, FriendlyTurrets.attackPassivesConfig.Value, FriendlyTurrets.attackTamedConfig.Value))
                 {
                     float num2 = Vector3.Distance(item.transform.position, me.position);
                     if (num2 < num || character == null)
@@ -113,39 +151,14 @@ namespace FriendlyTurrets
                     }
                 }
             }
-
+            if (FriendlyTurrets.debugLogging)
+            {
+                if (character != null) logger.LogMessage($"{character} has been chosen as the target!");
+            }
             return character;
         }
 
-        static bool IsValidTarget(Character item, bool log)
-        {
-            if (item.IsDead())
-            {
-                if(log)
-                    logger.LogMessage($"{item} is dead");
-                return false;
-            }
-            if (item is Player)
-            {
-                if (log)
-                    logger.LogMessage($"{item} is player");
-                return false;
-            }
-            if (item.IsTamed())
-            {
-                if (log)
-                    logger.LogMessage($"{item} is tamed creature");
-                return false;
-            }
 
-            if (item.GetComponents<Growup>().Any())
-            {
-                if (log)
-                    logger.LogMessage($"{item} is baby creature");
-                return false;
-            }
-            return true;
-        }
 
     }
 }
