0555ed948cfc54e0ba38f43c24cd724f91716176
Me <rhgs77@yahoo.com>
2024-05-16 21:48:54 -0400

    Fixed for 0.218.15 there was a new value in the find closest creature function.  Also of note, there is a bool in valheim proper now that makes this mod redudant EXCEPT that accessing it needs a mod i think.

--- FriendlyTurrets.cs
+++ FriendlyTurrets.cs
@@ -19,13 +19,13 @@ namespace FriendlyTurrets
     {
         private readonly Harmony harmony = new Harmony("ByteArtificer.FriendlyTurrets");
         public static ManualLogSource logger;
-        public static bool debugLogging = false;
+        //public static bool debugLogging = false;
 
         private static ConfigEntry<bool> attackPassivesConfig;
         private static ConfigEntry<bool> attackPlayersConfig;
         private static ConfigEntry<bool> attackTamedConfig;
         private static ConfigEntry<bool> attackBabiesConfig;
-
+        private static ConfigEntry<bool> printDebugOutput;
         public void Awake()
         {
             logger = Logger;
@@ -35,9 +35,9 @@ namespace FriendlyTurrets
             attackPassivesConfig = Config.Bind("General",      // The section under which the option is shown
                                 "attackPassivesConfig",  // The key of the configuration option in the configuration file
                                 false, // The default value
-                                "True means passive creatures get shot at"); // Description of the option to show in the config file
+                                "True means passive creatures (deer and such) get shot at"); // Description of the option to show in the config file
 
-                        attackPlayersConfig = Config.Bind("General",
+            attackPlayersConfig = Config.Bind("General",
                                                 "attackPlayersConfig",
                                                 false,
                                                 "True means Players get shot at");
@@ -50,6 +50,12 @@ namespace FriendlyTurrets
                                   "attackBabiesConfig",
                                   false,
             "True means baby creatures get shot at");
+
+            printDebugOutput = Config.Bind("Debug",
+                                  "printDebug",
+                                  false,
+            "True means spam in the log to help see why its busted - most players will not want to mess with this");
+
         }
 
         [HarmonyPatch(typeof(Turret), nameof(Turret.UpdateTarget))]
@@ -75,9 +81,10 @@ namespace FriendlyTurrets
 
                     if (methodInfo.Name == nameof(BaseAI.FindClosestCreature))
                     {
-                        logger.LogMessage($"Patching Successful! {j}");
+                        logger.LogMessage($"Patching {j}");
                         foundClosestCreatureCall = true;
                         codes[j] = new CodeInstruction(OpCodes.Call, typeof(FriendlyTurrets).GetMethod(nameof(FriendlyTurrets.FindClosestPlayerUnfriendlyCreature)));
+                        logger.LogMessage($"Probably Succeeded");
                         break;
                     }
                 }
@@ -90,68 +97,97 @@ namespace FriendlyTurrets
         }
 
 
-        public static Character FindClosestPlayerUnfriendlyCreature(Transform me, Vector3 eyePoint, float hearRange, float viewRange, float viewAngle, bool alerted, bool mistVision, bool passiveAggresive, bool includePlayers, bool includeTamed, List<Character> onlyTargets = null)
+        public static Character FindClosestPlayerUnfriendlyCreature(Transform me, Vector3 eyePoint, float hearRange, float viewRange, float viewAngle, bool alerted, bool mistVision, bool passiveAggresive, bool includePlayers, bool includeTamed, bool includeEnemies = true, List<Character> onlyTargets = null)
         {
 
 
             List<Character> allCharacters = Character.GetAllCharacters();
             Character character = null;
             float num = 99999f;
+
+            // i dont know exactly what this IF does but its in the 0.218.15 so ill put it here too
+            if (!includeEnemies && ZoneSystem.instance.GetGlobalKey(GlobalKeys.PassiveMobs))
+            {
+                WearNTear component = ((Component)me).GetComponent<WearNTear>();
+                if (component != null && component.GetHealthPercentage() == 1f)
+                {
+                    return null;
+                }
+            }
+
             foreach (Character item in allCharacters)
             {
+
+
+                bool flag = item is Player;
+
+                if (FriendlyTurrets.attackPlayersConfig.Value && flag || (!includeEnemies && !flag) || (!FriendlyTurrets.attackTamedConfig.Value && item.IsTamed()) || (!FriendlyTurrets.attackBabiesConfig.Value && item.GetComponents<Growup>().Any()) || item.IsDead())
+                {
+
+                    if (FriendlyTurrets.printDebugOutput.Value) logger.LogMessage(
+                        $"{item}  FriendlyTurrets.attackPlayersConfig.Value=" + FriendlyTurrets.attackPlayersConfig.Value 
+                        +" FriendlyTurrets.attackTamedConfig.Value=" + FriendlyTurrets.attackTamedConfig.Value
+                        + " FriendlyTurrets.attackBabiesConfig.Value=" + FriendlyTurrets.attackBabiesConfig.Value
+                        + " (!attackPlayers && item is Player)=" + (!FriendlyTurrets.attackPlayersConfig.Value && item is Player)
+                        + " includeEnemies=" + includeEnemies
+                        + " (!FriendlyTurrets.attackTamedConfig.Value  && item.IsTamed())=" + (!FriendlyTurrets.attackTamedConfig.Value && item.IsTamed())
+                        + " is dead=" + item.IsDead()
+                    );
+
+                    continue;
+                }
+
+             
+
                 BaseAI baseAI = item.GetBaseAI();
 
 
-                if (FriendlyTurrets.debugLogging) logger.LogMessage(
-                    $"{item}  FriendlyTurrets.attackPlayersConfig.Value=" + FriendlyTurrets.attackPlayersConfig.Value +
-                    " FriendlyTurrets.attackPassivesConfig.Value=" + FriendlyTurrets.attackPassivesConfig.Value
-                    + " FriendlyTurrets.attackTamedConfig.Value=" + FriendlyTurrets.attackTamedConfig.Value
-                    + " FriendlyTurrets.attackBabiesConfig.Value=" + FriendlyTurrets.attackBabiesConfig.Value
-                    + " baseAI=" + baseAI +
-                    " (!attackPlayers && item is Player)=" + (!FriendlyTurrets.attackPlayersConfig.Value && item is Player) +
-                    " (!FriendlyTurrets.attackTamedConfig.Value  && item.IsTamed())=" + (!FriendlyTurrets.attackTamedConfig.Value && item.IsTamed()) +
-                    " item.GetBaseAI().m_passiveAggresive)=" + (baseAI ? baseAI.m_passiveAggresive : false));
+               
 
-                if (baseAI == null || (!FriendlyTurrets.attackPassivesConfig.Value && baseAI.m_passiveAggresive) || (!FriendlyTurrets.attackTamedConfig.Value && item.IsTamed()) || (!FriendlyTurrets.attackPlayersConfig.Value && item is Player) || (!FriendlyTurrets.attackBabiesConfig.Value && item.GetComponents<Growup>().Any()))
+                if (baseAI == null || (!FriendlyTurrets.attackPassivesConfig.Value && baseAI.m_passiveAggresive)  )
                 {
+                   if (FriendlyTurrets.printDebugOutput.Value) logger.LogMessage(
+                   $"{item} FriendlyTurrets.attackPassivesConfig.Value=" + FriendlyTurrets.attackPassivesConfig.Value
+                   + " baseAI=" + baseAI
+                   + " item.GetBaseAI().m_passiveAggresive)=" + (baseAI ? baseAI.m_passiveAggresive : false));
+
                     continue;
                 }
 
-                if (item.IsDead()) // dont beat a dead horse
-                {
-                    continue;
-                }
 
                 if (onlyTargets != null && onlyTargets.Count > 0)
                 {
-                    bool flag = false;
+                    bool flag2 = false;
                     foreach (Character onlyTarget in onlyTargets)
                     {
                         if (item.m_name == onlyTarget.m_name)
                         {
-                            flag = true;
+                            flag2 = true;
                             break;
                         }
                     }
-                    if (!flag)
+                    if (!flag2)
                     {
                         continue;
                     }
                 }
-                
 
 
-                if ((!(baseAI != null) || !baseAI.IsSleeping()) && BaseAI.CanSenseTarget(me, eyePoint, hearRange, viewRange, viewAngle, alerted, mistVision, item, FriendlyTurrets.attackPassivesConfig.Value, FriendlyTurrets.attackTamedConfig.Value))
+                if ((!((Object)(object)baseAI != (Object)null) || !baseAI.IsSleeping()) && BaseAI.CanSenseTarget(me, eyePoint, hearRange, viewRange, viewAngle, alerted, mistVision, item, FriendlyTurrets.attackPassivesConfig.Value, FriendlyTurrets.attackTamedConfig.Value))
                 {
-                    float num2 = Vector3.Distance(item.transform.position, me.position);
-                    if (num2 < num || character == null)
+                    float num2 = Vector3.Distance(((Component)item).transform.position, me.position);
+                    if (num2 < num || (Object)(object)character == (Object)null)
                     {
                         character = item;
                         num = num2;
                     }
                 }
+
+
+
+
             }
-            if (FriendlyTurrets.debugLogging)
+            if (FriendlyTurrets.printDebugOutput.Value)
             {
                 if (character != null) logger.LogMessage($"{character} has been chosen as the target!");
             }
